20e48e8ec4ec8215572960f08f146ba7b03c8130
Bea Aira Lee Corpuz <bcorpuz.cloudpanda@gmail.com>
2022-09-10 12:14:02 +0800

    [chore] updated app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
 const App = () => {
+  const [traveler, setTraveler] = useState('traveler')
+
+  const handleChange = (e) => {
+    setTraveler(e.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ad astra, abyssosque!</h1>
+        <p>Hi, {traveler}!</p>
+        <input
+          name="traveler"
+          type="text"
+          placeholder="Enter traveler name" 
+          onChange={handleChange}
+        />
+        <p>
+          Edit <code>App.js</code> and save to test updates.
+        </p>
       </header>
     </div>
   )
